9c9cc8b0b3fc02c734b0adebb30a0fdc03ebaf17
lx <lqu3qf@gmail.com>
2024-05-31 17:06:29 +0800

    feat: add tags

--- pkg/model/add_torrents_options.go
+++ pkg/model/add_torrents_options.go
@@ -25,4 +25,6 @@ type AddTorrentsOptions struct {
 	SequentialDownload bool `json:"sequentialDownload,omitempty"`
 	// Prioritize download first last piece. Possible values are true, false (default)
 	FirstLastPiecePrio bool `json:"firstLastPiecePrio,omitempty"`
+	// Tags, split by ','
+	Tags string `json:"tags,omitempty"`
 }
